697cbd0e494781562d3927f69a5d70061209b0bf
Cody C. Sykes <kodye45@gmail.com>
2013-03-29 22:53:45 -0600

    all folders added master build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockstore::Application.config.secret_token = '5fa1c6c1a0badbb90647887e6caf5db0d25237d9229bae1a4d7c96dd525cb90d6dc62016da5efafc0369516ff9233547b9fce74cc246f0e1ce9029cd0ed2934b'
